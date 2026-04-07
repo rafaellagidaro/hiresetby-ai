6cc14c30986e757c159cbae9645acb7dfec9b53b
Bruna Rafaela Gidaro <brgidaro@hotmail.com>
2026-04-07 15:10:39 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,5 @@
 from sqlalchemy import create_engine
 
 engine = create_engine("sqlite:///app.db")
-🗄️ models_db.py
-Python
-from sqlalchemy import Column, Integer, Float
-from sqlalchemy.orm import declarative_base
 
-Base = declarative_base()
 
-class Candidate(Base):
-    _tablename_ = "candidates"
-
-    id = Column(Integer, primary_key=True)
-    tempo_resposta = Column(Float)
-    feedback_bin = Column(Integer)
-    score_curriculo = Column(Float)
-    prediction = Column(Integer)
